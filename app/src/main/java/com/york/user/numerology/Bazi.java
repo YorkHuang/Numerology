e6e98d8277bb8f6a405be579bc6d576f021a915f
York <wagaluyo@gmail.com>
2016-11-09 18:14:26 +0800

    upload

---
+++ app/src/main/java/com/york/user/numerology/Bazi.java
@@ -0,0 +1,17 @@
+package com.york.user.numerology;
+
+/**
+ * Created by GRT-EK on 2016/11/9.
+ */
+
+public class Bazi {
+
+    private int[] mLifeCode;
+
+    public Bazi(int[]  lifeCode) {
+        this.mLifeCode = lifeCode;
+    }
+
+
+
+}
